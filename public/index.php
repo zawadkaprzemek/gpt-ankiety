3e72b9e60765822549aa2a2698baf69b44287cfb
Przemysław Zawadka <zawadkaprzemek@gmail.com>
2022-07-10 00:20:58 +0200

    timezone set

--- public/index.php
+++ public/index.php
@@ -5,6 +5,6 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    var_dump(date_default_timezone_get());die;
+    var_dump(date_default_timezone_set("Europe/Warsaw"));
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
